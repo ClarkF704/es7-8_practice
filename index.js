c16a777251880eeeb477275af2039d1e10198fde
Clark Farriss <clarkfarriss@Clarks-MBP.lan1>
2019-07-04 12:21:14 -0400

    a little something

---
+++ index.js
@@ -0,0 +1,39 @@
+// padStart padEnd
+const string = '12345';
+
+console.log(string.padStart(10, '.'));
+console.log(string.padEnd(10, '.'));
+
+// Usage of padStart --> Prepending area code to phone number if the user length isint the correct length
+// Usage of padEnd --> decimal precision if you want to add decimal points
+
+// Object.values
+
+const object = {
+    name: 'John',
+    age: '20',
+    favoriteBooks: ['Rich Dad Poor Dad', 'Art of the deal']
+}
+
+console.log(Object.values(object));
+
+// Object.entries
+
+console.log(Object.entries(object));
+
+// Async functions
+
+
+// Math Powers
+console.log(Math.pow(2, 3))
+
+// Trailing commas
+
+const anotherObject ={
+    first: 1,
+    sec: 2,
+    third: 3
+}
+
+console.log(anotherObject);
+
